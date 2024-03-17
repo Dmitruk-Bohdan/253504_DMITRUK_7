3ca7a75829eeec5e8c50667ecf1d43c6ce0e5496
KirillKurril <kirillkurill1525@gmail.com>
2024-03-17 17:42:21 +0300

    q

---
+++ IGI/LR2/my-image-source/script.py
@@ -0,0 +1,24 @@
+import os, sys
+
+current_dir = os.getcwd()
+
+parent_dir = os.path.dirname(current_dir)
+
+module_dir = os.path.join(parent_dir, "volume", "geometric_lib")
+
+sys.path.append(module_dir)
+
+import square
+
+value = int(os.getenv("SQUARE_SIDE"))
+
+print(f"square side equals: {value}")
+
+square_area = square.area(value)
+
+print(square_area)
+
+env_vars = os.environ
+
+for var in env_vars:
+    print(var, "=", env_vars[var])
