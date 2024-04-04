b9483519f6ae4da917a7edbf771165df6c059dc7
KirillKurril <kirillkurill1525@gmail.com>
2024-04-05 02:33:29 +0300

    commit

--- IGI/LR4/text_handler.py
+++ IGI/LR4/text_handler.py
@@ -2,16 +2,24 @@ import zipfile
 
 class TextHandler:
     def __init__(self):
-        pass
+        self.text = str()
 
-    def read_from_file(file_path : str):
-        pass
+    def read_from_file(self, file_path : str):
+        with open(file_path, 'r') as file:
+            self.text = file.read()
 
-    def save_to_file(file_path : str):
-        pass
+    def save_to_file(self, file_path : str):
+        with open(file_path, 'w') as file:
+            file.write(self.text)
 
-    def archive ():
-        pass
+    def archive(file_path, zip_file_path):
+
+        with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
+            zip_file.write(file_path, arcname=file_path.split('/')[-1])
+    
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_file:
+            return zip_file.getinfo(file_path.split('/')[-1])
+
 
     def get_stat_as_file():
         pass
