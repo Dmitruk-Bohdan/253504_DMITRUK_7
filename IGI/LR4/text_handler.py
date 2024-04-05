f70d8b3685edc7602a40c07eb3ca2fde2ae292be
KirillKurril <kirillkurill1525@gmail.com>
2024-04-05 14:05:36 +0300

    commit

--- IGI/LR4/text_handler.py
+++ IGI/LR4/text_handler.py
@@ -1,4 +1,7 @@
+from functools import reduce
+import string
 import zipfile 
+import re
 
 class TextHandler:
     def __init__(self):
@@ -24,11 +27,36 @@ class TextHandler:
     def get_stat_as_file():
         pass
 
+    def get_sentences_count(string : str):
+        return re.findall(r"([^.!?]+[.!?])", string)
+    
+    def get_sentrnces_count_by_kinds(string : str):
+        narrative_sentences = re.findall(r"([^.!?]+\.)", string)
+        interrogative_sentences = re.findall(r"([^.!?]+\?)", string)
+        imperative_sentences = re.findall(r"([^.!?]+\!)", string)
+        return narrative_sentences, interrogative_sentences, imperative_sentences
+    
+    def average_sentence_length(string : str):
+        sentences = re.split(r"([^.!?]+[.!?])", string)
+        return (int)(reduce(lambda x, y : x + y, [len(re.sub(r"[^\w]", "", sentence)) for sentence in sentences]) / len(sentences))
+    
+    def average_word_length(string : str):
+        words = re.findall(r"(\w+)", string)
+        return (int)(reduce(lambda x, y : x + y, [len(word) for word in [words]]) / len(words))
+    
+    def get_emoticon_count(string : str):
+        return len([re.findall(r"[;:]-*[()[]]+")])
+
     def get_low_case_and_numbers_words():
-        pass
+        words = re.findall(r"(\w+)", string)
+        return re.findall(r"(?:^|\s)(\w*[[a-z][0-9]]\w*)(?:$|\s|[!?.,(...)])")
 
     def is_ip():
         pass
 
-    
+    def remove_punctuation_and_spaces(text):
+        punctuation_and_spaces = string.punctuation + " "
+        for char in punctuation_and_spaces:
+            text = text.replace(char, "")
+        return text
 
