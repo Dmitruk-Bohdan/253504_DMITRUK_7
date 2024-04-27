e05f9ee3cc160bf18f3d0709124937b96b4cac60
KirillKurril <kirillkurill1525@gmail.com>
2024-04-27 14:53:02 +0300

    встроенные таблицы и панель администрирования

--- IGI/LR5/DjangoProject/autoshop/autoshop/mainapp/admin.py
+++ IGI/LR5/DjangoProject/autoshop/autoshop/mainapp/admin.py
@@ -1,10 +1,57 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Manufacturer)
-admin.site.register(Supplier)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Customer)
-admin.site.register(Sale)
 
+@admin.register(PickupPoint)
+class PickupPointAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'phone_number']
+    list_filter = ['name', 'address', 'phone_number']
+
+@admin.register(PromoCode)
+class PromoCodeAdmin(admin.ModelAdmin):
+    list_display = ['code']
+    list_filter = ['code']
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'phone']
+    list_filter = ['name']
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'phone']
+    list_filter = ['name']
+
+class SupplierInline(admin.StackedInline):
+    model = Supplier.products.through
+    
+    extra = 0
+    verbose_name = 'Supplier'
+    verbose_name_plural = 'Suppliers'
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name',
+                    'article_number',
+                    'description',
+                    'price_per_unit',
+                    'category',
+                    'display_suppliers',
+                    'count'
+                    ]
+    inlines = [SupplierInline]
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ['date', 'quantity', 'product', 'customer']
+    list_filter = ['date', 'quantity']
--- IGI/LR5/DjangoProject/autoshop/autoshop/mainapp/models.py
+++ IGI/LR5/DjangoProject/autoshop/autoshop/mainapp/models.py
@@ -1,12 +1,10 @@
-import uuid
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Manufacturer(models.Model):
     #id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
+    address = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     
     def __str__(self):
@@ -14,7 +12,7 @@ class Manufacturer(models.Model):
 
 class Supplier(models.Model):
     name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
+    address = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     manufacturers = models.ManyToManyField(Manufacturer)
 
@@ -33,12 +31,17 @@ class Product(models.Model):
     description = models.TextField(max_length=100)
     price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
     suppliers = models.ManyToManyField(Supplier, related_name ='products')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     count = models.IntegerField()
 
     @property
     def total_price(self):
         return self.count * self.price_per_unit
+    
+    def display_suppliers(self):
+        return ', '.join([ supplier.name for supplier in self.suppliers.all()[:3]])
+    
+    display_suppliers.short_description = 'Suppliers'
 
     def __str__(self):
         return self.name
@@ -65,65 +68,14 @@ class Sale(models.Model):
     def __str__(self):
         return f"Sale: {self.product.name} - {self.quantity} units"
     
+class PickupPoint(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
     
-# class CustomUserManager(BaseUserManager):
-#     def create_user(self, email, password=None, **extra_fields):
-#         if not email:
-#             raise ValueError('The Email field must be set')
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#         user.save()
-#         return user
-
-#     def create_superuser(self, email, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         return self.create_user(email, password, **extra_fields)
-
-
-# class CustomUser(AbstractBaseUser, PermissionsMixin):
-#     email = models.EmailField(unique=True)
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-
-#     objects = CustomUserManager()
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['first_name', 'last_name']
-
-#     class Meta:
-#         # Используйте related_name для избежания конфликтов обратного доступа
-#         # с полями groups и user_permissions модели User
-#         # встроенной модели пользователя Django.
-#         # Назовите related_name с учетом вашего проекта и логики доступа.
-#         # Например, 'custom_user_groups' и 'custom_user_permissions'.
-#         # Или выберите другие подходящие имена.
-#         related_query_name = 'custom_user_%(class)s'
-
-#     def get_full_name(self):
-#         return f'{self.first_name} {self.last_name}'
-
-#     def get_short_name(self):
-#         return self.first_name
-
-
-# class Moderator(CustomUser):
-#     class Meta:
-#         proxy = True
-
-#     def save(self, *args, **kwargs):
-#         self.is_staff = True
-#         super().save(*args, **kwargs)
-
-
-# class SuperUser(CustomUser):
-#     class Meta:
-#         proxy = True
-
-#     def save(self, *args, **kwargs):
-#         self.is_staff = True
-#         self.is_superuser = True
-#         super().save(*args, **kwargs)
+class PromoCode(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    users = models.ManyToManyField(User)    
